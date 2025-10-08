ab3fa7ec7980e748ae2dd8a5dd99e618bcff38f3
Pedro Henrique Ferreira Carmeiro <pedrohenrique00779@gmail.com>
2025-10-08 08:15:10 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def criar_pessoa(nome: str, idade: int, id: int) -> dict:
+    return {
+        "id": id,
+        "nome": nome,
+        "idade": idade
+    }
+
+pessoa1 = criar_pessoa("João", 30, 1)
+pessoa2 = criar_pessoa("Pedro", 18, 2)
+pessoa3 = criar_pessoa("Guilherme", 18, 3)
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
